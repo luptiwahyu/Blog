46a29043e82540bfd8aade4455e08a3906f7a20b
Lupti Wahyu <luptiwahyu@ymail.com>
2016-02-04 14:07:48 +0700

    Update development.php

--- app/config/development.php
+++ app/config/development.php
@@ -20,9 +20,9 @@ return [
     'db' => [
         'driver'    => 'mysql',
         'host'      => 'localhost',
-        'database'  => 'blogslim',
-        'username'  => 'kid',
-        'password'  => 'sequel',
+        'database'  => 'dbname',
+        'username'  => 'root',
+        'password'  => '',
         'charset'   => 'utf8',
         'collation' => 'utf8_unicode_ci',
         'prefix'    => ''
